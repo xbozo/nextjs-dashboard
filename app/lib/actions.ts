a2f81ed82fe4e0939b0d147913185b8a749731c3
Guilherme V. <imluizguilara@hotmail.com>
2023-11-16 09:25:44 -0300

    Chapter 15 - Adicionando autenticação

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { signIn } from '@/auth'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
@@ -28,6 +29,17 @@ export type State = {
 		status?: string[]
 	}
 	message?: string | null
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+	try {
+		await signIn('credentials', Object.fromEntries(formData))
+	} catch (error) {
+		if ((error as Error).message.includes('CredentialsSignin')) {
+			return 'CredentialSignin'
+		}
+		throw error
+	}
 }
 
 export async function createInvoice(prevState: State, formData: FormData) {
