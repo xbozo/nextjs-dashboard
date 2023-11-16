e79997f5da993aac0c1ef46094bf66dc232287a9
Guilherme V. <imluizguilara@hotmail.com>
2023-11-16 09:38:17 -0300

    Chapter 16 - Adicionando metadados

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
-import "./ui/global.css";
-import { inter } from "@/app/ui/fonts";
+import { Metadata } from 'next'
+import './ui/global.css'
+import { inter } from '@/app/ui/fonts'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Acme Dashboard',
+		default: 'Acme Dashboard',
+	},
+	description: 'The official Next.js Learn Dashboard built with App Router.',
+	metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	)
 }
